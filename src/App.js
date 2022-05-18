94cc7de9c3b06d2487b320c864fd977d1aa68b78
saurabh <saurabhkedar@gmail.com>
2022-05-18 19:25:48 +0530

    added the routes and developed core page structures

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Navbar from './Components/Layouts/Navbar'
 import Footer from './Components/Layouts/Footer'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import About from './Pages/About'
+import Home from './Pages/Home'
+import NotFound from './Pages/NotFound'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main className='container mx-auto px-3 pb-13'>Content</main>
+        <main className='container mx-auto px-3 pb-13'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/About' element={<About />} />
+            <Route path='/NotFound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
---
+++ src/Pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>Git-SnooF</h1>
+      <h2 className='text-gray text-xl'>by</h2>
+      <h2 className='text-gray text-xl'>Saurabh Kedar</h2>
+    </div>
+  )
+}
+
+export default About
